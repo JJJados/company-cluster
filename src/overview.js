84a32e351bedbd20ed51b18e0ed95c9365e79887
jaydenlaturnus <jaydenlaturnus@gmail.com>
2019-03-15 13:52:26 -0600

    removed comments

--- src/overview.js
+++ src/overview.js
@@ -36,69 +36,10 @@ d3.json("http://159.89.155.66:8080/api/v1/dividends?date=2019-03", function(erro
         .range(["#92b9bd", "#a8d4ad", "#e8ec67", "#8e5572", "#91f5ad", "#745296", 
                 "#ee6c4d", "#4CAF50", "#d4b483", "#ff01fb", "#f28123"]);
 
-    /*var x = d3.scaleOrdinal()
-        .domain(sectors)
-        .range([0, w_multi, w_multi*2, w_multi*3, w_multi*4, w_multi*5, w_multi*6, w_multi*7, w_multi*8, w_multi*9, w_multi*10, width])
-    
-    var y = d3.scaleOrdinal()
-        .domain(sectors)
-        .range([height/3, height/3, height/3, height/2, height/2, height/2, height/4, height/4, height/4]);
-    */
-
     // Size scale for market cap
     var size = d3.scaleLinear()
         .domain([0, 130000])
         .range([10,22])  // circle will be between 7 and 55 px wide
-
-    var fontSize = d3.scaleLinear()
-        .domain([0, 130000])
-        .range([5, 20])
-
-    /*var node = svg.selectAll(null)
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("class", "nodes")
-
-    node.append("circle")
-        .attr("r", function(d) { 
-            if (isNaN(size(d.fundamental["MktCap (dollarsMil)"]))) {
-                return 0
-            }
-            return size(d.fundamental["MktCap (dollarsMil)"])
-        })
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
-        .style("fill", function(d) { 
-            return color(d.general.Sector) 
-        })
-        .style("fill-opacity", 0.8)
-        .attr("stroke", "black")
-        .style("stroke-width", 1)
-        .on("mouseover", mouseover) // What to do when hovered
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
-        .call(d3.drag() // call specific function when circle is dragged
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended))
-    node.append("text")
-            //.attr("class", "label")
-            .attr("fill", "black")
-            .style("text-anchor", "middle")
-            .text(function(d) {
-                return d.general["Ticker Symbol"]
-            })
-            .style("font-size", function(d) {
-                if (isNaN(size(d.fundamental["MktCap (dollarsMil)"]))) {
-                    return 0
-                }
-                return size(d.fundamental["MktCap (dollarsMil)"])
-            })
-            //.style("stroke-width", 1)
-            .attr("x", width/2)
-            .attr("y", height/2)
-    */
 
     // Three function that change the tooltip when user hover / move / leave a cell
     var mouseover = function(d) {
@@ -147,11 +88,9 @@ d3.json("http://159.89.155.66:8080/api/v1/dividends?date=2019-03", function(erro
         .on("mousemove", mousemove)
         .on("mouseleave", mouseleave)
         .call(d3.drag() // call specific function when circle is dragged
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended))
-
-
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended))
 
     // Features of the forces applied to the nodes:
     var simulation = d3.forceSimulation()
@@ -169,27 +108,13 @@ d3.json("http://159.89.155.66:8080/api/v1/dividends?date=2019-03", function(erro
         .nodes(data)
         .on("tick", function(d){
             node
-                /*.attr("transform", function(d) {
-
-                    return "translate("+ d.x / 2 +"," + d.y / 2 + ")"
-                });*/
-            .attr("cx", function(d) { 
-                return d.x = Math.max(radius, Math.min(width - radius, d.x)); 
-            })
-            .attr("cy", function(d){ 
-                return d.y = Math.max(radius, Math.min(height - radius, d.y)); 
-            })
+                .attr("cx", function(d) { 
+                    return d.x = Math.max(radius, Math.min(width - radius, d.x)); 
+                })
+                .attr("cy", function(d){ 
+                    return d.y = Math.max(radius, Math.min(height - radius, d.y)); 
+                })
         })
-        /*.on("tick", function(d) {
-            node
-            .attr("x", function(d) { 
-                return d.x = Math.max(radius, Math.min(width - radius, d.x)); 
-            })
-            .attr("y", function(d){ 
-                return d.y = Math.max(radius, Math.min(height - radius, d.y)); 
-            })
-        })*/
-
 
     // What happens when a circle is dragged?
     function dragstarted(d) {
